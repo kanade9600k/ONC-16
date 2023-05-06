0746133a593241c0d24bf53e6c7de39b572343ff
kanade9600k <tomo9600k@gmail.com>
2023-05-06 19:24:09 +0900

    フラグのインデックスを定数化

--- alu.v
+++ alu.v
@@ -20,17 +20,17 @@ module alu (
     // funcによって機能を分岐（組み合わせ回路）
     always @(*) begin
         case (func)
-            `ALU_ADD: {tmp_flags[1], tmp_y} <= a + b;
-            `ALU_SUB: {tmp_flags[1], tmp_y} <= a - b;
+            `ALU_ADD: {tmp_flags[`C_FLAG], tmp_y} <= a + b;
+            `ALU_SUB: {tmp_flags[`C_FLAG], tmp_y} <= a - b;
             `ALU_TH:  tmp_y <= b;
             `ALU_NOT: tmp_y <= ~b;
             `ALU_AND: tmp_y <= a & b;
             `ALU_OR:  tmp_y <= a | b;
             `ALU_XOR: tmp_y <= a ^ b;
             // シフトは最後のあふれがキャリー，桁あふれを保持するため1bit拡張して演算
-            `ALU_SRA: {tmp_y, tmp_flags[1]} <= ($signed({a, 1'b0}) >>> b);
-            `ALU_SRL: {tmp_y, tmp_flags[1]} <= ({a, 1'b0}) >> b;
-            `ALU_SLL: {tmp_flags[1], tmp_y} <= ({1'b0, a}) << b;
+            `ALU_SRA: {tmp_y, tmp_flags[`C_FLAG]} <= ($signed({a, 1'b0}) >>> b);
+            `ALU_SRL: {tmp_y, tmp_flags[`C_FLAG]} <= ({a, 1'b0}) >> b;
+            `ALU_SLL: {tmp_flags[`C_FLAG], tmp_y} <= ({1'b0, a}) << b;
             default: begin
                 tmp_y <= `DATA_W'bx;
                 tmp_flags <= `FR_FLAG_W'b0;
@@ -40,10 +40,10 @@ module alu (
 
     // 出力割当て
     assign y = tmp_y;
-    assign flags[3] = y[`DATA_W-1];  // ネガティブフラグ
-    assign flags[2] = ~(|y);  // ゼロフラグ
-    assign flags[1] = tmp_flags[1];  // キャリーフラグ
-    assign flags[0] = (  // オーバーフローフラグ，同符号同士の加算結果が異符号，正-負の結果が負，負-正の結果が正の場合1
+    assign flags[`N_FLAG] = y[`DATA_W-1];  // ネガティブフラグ
+    assign flags[`Z_FLAG] = ~(|y);  // ゼロフラグ
+    assign flags[`C_FLAG] = tmp_flags[1];  // キャリーフラグ
+    assign flags[`V_FLAG] = (  // オーバーフローフラグ，同符号同士の加算結果が異符号，正-負の結果が負，負-正の結果が正の場合1
         ((func == `ALU_ADD) && ((!a[`DATA_W-1] && !b[`DATA_W-1] && y[`DATA_W-1]) || (a[`DATA_W-1] && b[`DATA_W-1] && !y[`DATA_W-1])))
      || ((func == `ALU_SUB) && ((!a[`DATA_W-1] && b[`DATA_W-1] && y[`DATA_W-1]) || (a[`DATA_W-1] && !b[`DATA_W-1] && !y[`DATA_W-1]))));
 
