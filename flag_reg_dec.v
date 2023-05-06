3650bcdf72cfd08abd9154f1869397ad2e70c10d
kanade9600k <tomo9600k@gmail.com>
2023-05-06 20:16:11 +0900

    フラグレジスタ・デコーダを実装（テスト済み）

---
+++ flag_reg_dec.v
@@ -0,0 +1,47 @@
+// フラグレジスタ・デコーダ
+`ifndef FLAG_REG_DEC_V
+`define FLAG_REG_DEC_V
+`include "def.v"
+`default_nettype none
+
+module flag_reg_dec (
+    input wire [`FR_FLAG_W-1:0] flags,
+    input wire [`FR_FUNC_W-1:0] func,
+    input wire bre,
+    input wire clock,
+    input wire n_rst,
+    output wire is_br
+);
+
+    // 内部信号
+    reg [`FR_FLAG_W-1:0] flag_reg;
+    reg tmp_dec;  //（reg型だが，always @(*) なので組み合わせ回路が生成される）
+
+    // 機能記述
+    // 順序回路（レジスタ部分）
+    always @(posedge clock or negedge n_rst) begin
+        if (!n_rst) begin
+            flag_reg <= `FR_FLAG_W'b0;
+        end else begin
+            flag_reg <= flags;
+        end
+    end
+
+    // 組み合わせ回路（デコーダ部分）
+    always @(*) begin
+        case (func)
+            `FR_BAL: tmp_dec <= 1'b1;  // 常に分岐
+            `FR_BEQ: tmp_dec <= flag_reg[`Z_FLAG];  // Z = 1で分岐
+            `FR_BNE: tmp_dec <= !flag_reg[`Z_FLAG];  // Z = 0で分岐
+            `FR_BLT: tmp_dec <= (!flag_reg[`N_FLAG] == flag_reg[`V_FLAG]);  // !N = Vで分岐
+            `FR_BGE: tmp_dec <= (flag_reg[`N_FLAG] == flag_reg[`V_FLAG]);  // N = Vで分岐
+            `FR_BLTU_BC: tmp_dec <= flag_reg[`C_FLAG];  // C = 1で分岐
+            `FR_BGEU_BNC: tmp_dec <= !flag_reg[`C_FLAG];  // C = 0で分岐
+            `FR_BVF: tmp_dec <= flag_reg[`V_FLAG];  // V = 1で分岐
+            default: tmp_dec <= 1'bx;
+        endcase
+    end
+    assign is_br = tmp_dec & bre;
+
+endmodule
+`endif
---
+++ flag_reg_dec_test.v
@@ -0,0 +1,81 @@
+// フラグレジスタ・デコーダのテストベンチ
+`ifndef FLAG_REG_DEC_TEST_V
+`define FLAG_REG_DEC_TEST_V
+`default_nettype none
+`include "def.v"
+`include "flag_reg_dec.v"
+`timescale 1ns / 1ns
+
+module flag_reg_dec_test;
+
+    // 入出力の宣言
+    reg [`FR_FLAG_W-1:0] flags_tb;
+    reg [`FR_FUNC_W-1:0] func_tb;
+    reg bre_tb;
+    reg clock_tb;
+    reg n_rst_tb;
+    wire is_br_tb;
+
+    // モジュールの宣言
+    flag_reg_dec flag_reg_dec_inst (
+        .flags(flags_tb),
+        .func (func_tb),
+        .bre  (bre_tb),
+        .clock(clock_tb),
+        .n_rst(n_rst_tb),
+        .is_br(is_br_tb)
+    );
+
+    // クロック記述
+    always #5 clock_tb <= ~clock_tb;
+
+    // テストパターン
+    integer i, j;
+    initial begin
+        clock_tb <= 1'b0;
+        n_rst_tb <= 1'b1;
+        #2;
+        for (i = 0; i < 2 ** `FR_FUNC_W; i = i + 1) begin
+            for (j = 0; j < 2 ** `FR_FLAG_W; j = j + 1) begin
+                bre_tb   <= 1'b1;
+                func_tb  <= i;
+                flags_tb <= j;
+                #10;
+            end
+        end
+        for (i = 0; i < 2 ** `FR_FUNC_W; i = i + 1) begin
+            for (j = 0; j < 2 ** `FR_FLAG_W; j = j + 1) begin
+                bre_tb   <= 1'b0;
+                func_tb  <= i;
+                flags_tb <= j;
+                #10;
+            end
+        end
+        n_rst_tb <= 1'b0;
+        for (i = 0; i < 2 ** `FR_FUNC_W; i = i + 1) begin
+            for (j = 0; j < 2 ** `FR_FLAG_W; j = j + 1) begin
+                bre_tb   <= 1'b1;
+                func_tb  <= i;
+                flags_tb <= j;
+                #10;
+            end
+        end
+        n_rst_tb <= 1'b1;
+        for (i = 0; i < 2 ** `FR_FUNC_W; i = i + 1) begin
+            for (j = 0; j < 2 ** `FR_FLAG_W; j = j + 1) begin
+                bre_tb   <= 1'b1;
+                func_tb  <= i;
+                flags_tb <= j;
+                #10;
+            end
+        end
+        $finish;
+    end
+
+    initial begin
+        $dumpfile("flag_reg_dec.vcd");
+        $dumpvars(0);
+    end
+
+endmodule
+`endif
