acc234356b80017c47654f5a3d37a5e99be163d6
kanade9600k <tomo9600k@gmail.com>
2023-07-16 22:13:24 +0900

    実質的に不要な変数を削除

--- uart_tx.v
+++ uart_tx.v
@@ -8,7 +8,7 @@ module uart_tx (
     input wire n_rst,
     input wire start,
     input wire [7:0] tx_data,
-    output wire ready,  // 送信準備完了: 1, 送信中or送信準備中: 0
+    output reg ready,  // 送信準備完了: 1, 送信中or送信準備中: 0
     output reg tx
 );
     // 内部定数定義
@@ -18,15 +18,14 @@ module uart_tx (
     reg [7:0] data_buf;  // 送信データの一時保存用
     reg [3:0] tx_index;  // どのビットを送っているか
     reg [8:0] clock_count;  // クロック分周用
-    reg is_send;  // 送信中(1: 送信中, 0: 待機中)
 
     always @(posedge clock_50M or negedge n_rst) begin
         if (!n_rst) begin  // リセット
             tx_index <= 4'b0;
             clock_count <= 5'd0;
-            is_send <= 1'b0;
+            ready <= 1'b1;
 
-        end else if (is_send) begin  // 送信中
+        end else if (!ready) begin  // 送信中
             if (clock_count == UART_CLOCK) begin  // URATのクロックタイミングの場合
                 clock_count <= 9'd0;
                 tx <= data_buf[0];  // 最下位ビットを送信
@@ -34,7 +33,7 @@ module uart_tx (
                 data_buf[7:0] <= {1'b1, data_buf[7:1]};  // 1ビット右シフト(左の1はストップビット)
 
                 if (tx_index == 4'd9) begin
-                    is_send <= 1'b0;  // 送信終了
+                    ready <= 1'b1;  // 送信終了
                 end
 
             end else begin  // URATのクロックタイミングでないとき
@@ -45,16 +44,13 @@ module uart_tx (
             clock_count <= 9'd0;
             data_buf <= tx_data;  // 入力からデータをコピー
             tx_index <= 4'd0;  // 送信データのインデックスを初期化
-            is_send <= 1'b1;  // 送信中に
+            ready <= 1'b0;  // 送信中に
             tx <= 1'b0;  // スタートビット
 
         end else begin
             tx <= 1'b1;  // 何もしないときは常時1を出力 
         end
     end
-
-    assign ready = !is_send;  // 送信中でない場合，送信準備完了信号を出力
-
 endmodule
 
 `endif
